39c674a08ee8693f68b2606eaf244337a1c08e9c
Gustavo Bazilio <guh9877@gmail.com>
2023-07-03 15:22:28 -0300

    inclusao chamada back end

--- back_end/back_end.py
+++ back_end/back_end.py
@@ -1,22 +1,30 @@
-import csv
+from flask import Flask, request
 import pandas as pd
 
-cabecalho = ["NOME","E-MAIL","SENHA","CPF","CELULAR","DT_NASC","RUA","NACIONALIDADE","CIDADE","BAIRRO","CEP"]
+app = Flask(__name__)
 
-nome = input('nome ')
-email = input('e-mail ')
-senha = input('senha ')
-cpf = input('cpf ')
-celular = input('celular ')
-dt_nasc = input('dt_nasc ')
-rua = input('rua ')
-nacionalidade = input('nacionalidade ')
-cidade = input('cidade ')
-bairro = input('bairro ')
-cep = input('cep ')
+@app.route('/salvar', methods=['POST'])
+def salvar():
+    dados = request.get_json()
+    nome = dados['nome']
+    email = dados['email']
+    senha = dados['senha']
+    cpf = dados['cpf']
+    dt_nasc = dados['dt_nasc']
+    celular = dados['celular']
+    rua = dados['rua']
+    nacionalidade = dados['nacionalidade']
+    cidade = dados['cidade']
+    bairro = dados['bairro']
+    cep = dados['cep']
 
-cadastro_pessoa = nome + '_cadastro'
-valor = [nome,email,senha,cpf,celular,dt_nasc,rua,nacionalidade,cidade,bairro,cep]
+    cabecalho = ["NOME", "E-MAIL", "SENHA", "CPF", "CELULAR", "DT_NASC", "RUA", "NACIONALIDADE", "CIDADE", "BAIRRO", "CEP"]
+    cadastro_pessoa = nome + '_cadastro'
+    valor = [nome, email,senha,cpf,celular,dt_nasc,rua,nacionalidade,cidade,bairro,cep]     
+    dff = pd.DataFrame([valor], columns=cabecalho)
+    dff.to_csv(f'{cadastro_pessoa}.csv', index=False)
 
-dff = pd.DataFrame([valor], columns=cabecalho)
-dff.to_csv(f'{cadastro_pessoa}.csv', index=False)
+    return '', 200
+
+if __name__ == '__main__':
+    app.run()
--- js/script.js
+++ js/script.js
@@ -46,10 +46,53 @@ function showFotos(n) {
   dots[slideIndex-1].className += " active";
 }
 
+document.getElementById('myForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  var nome = document.getElementById('nome').value;
+  var email = document.getElementById('email').value;
+  var senha = document.getElementById('senha').value;
+  var senha = document.getElementById('cf').value;
+  var senha = document.getElementById('dt_nasc').value;
+  var senha = document.getElementById('celular').value;
+  var senha = document.getElementById('rua').value;
+  var senha = document.getElementById('nacionalidade').value;
+  var senha = document.getElementById('cidade').value;
+  var senha = document.getElementById('bairro').value;
+  var senha = document.getElementById('cep').value;
+  var dados = {
+    
+  nome: nome,
+  email: email,
+  senha: senha,
+  cpf: cpf,
+  dt_nasc: dt_nasc,
+  celular:  celular,
+  rua: rua,
+  nacionalidade: nacionalidade,
+  cidade: cidade,
+  bairro: bairro,
+  cep: cep,
 
 
-   
 
+  };
 
-
-
+  fetch('/salvar', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dados)
+  })
+  .then(function(response) {
+    if (response.ok) {
+      alert('Dados salvos com sucesso!');
+    } else {
+      alert('Ocorreu um erro ao salvar os dados.');
+    }
+  })
+  .catch(function(error) {
+    console.error('Ocorreu um erro:', error);
+  });
+});
